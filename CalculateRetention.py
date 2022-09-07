207b91fc0fe7d1945d3a29dc9062e01bb51dedcb
Nathan Allen <nallen@indybbb.org>
2022-09-07 16:57:14 -0400

    made requested changes to formula, added fomatting for inCollections

--- CalculateRetention.py
+++ CalculateRetention.py
@@ -1,12 +1,10 @@
 from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
-import tkinter
 from tkinter import filedialog
 import os
 
 
 #get the workbook filepath from the user, dialog fileselect window
-workbookFilePath = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select the Excel File", filetypes=(("Excel Files", "*.xlsx"),("All Files", "*.*")))
+workbookFilePath = filedialog.askopenfilename(initialdir="C:/", title="Select the Excel File", filetypes=(("Excel Files", "*.xlsx"),("All Files", "*.*")))
 #open the workbook
 workbook = load_workbook(workbookFilePath)
 #get the active sheet
@@ -17,25 +15,31 @@ maxRows = sheet.max_row
 #get total dues collected
 #sheet[f"A{maxRows+1}"] = "=SUM(IF($F$2:$F${maxRows}=FALSE,$I$2:$I${maxRows},0))+SUM(IF(K2:K{maxRows}<>0,I2:I{maxRows},0))"
 
-#total Billed
-sheet[f"A{maxRows+1}"] = "Total Billed"
-sheet[f"B{maxRows+1}"] = f"=SUM(I2:I{maxRows})"
-#writtend Off
-sheet[f"A{maxRows+2}"] = "Written Off"
-sheet[f"B{maxRows+2}"] = f"=SUM(IF($F$2:$F${maxRows}=FALSE,$I$2:$I${maxRows},0))+SUM(IF(K2:K{maxRows}<>0,I2:I{maxRows},0))"
-sheet[f"C{maxRows+2}"] = f"=B{maxRows+2}/B{maxRows+1}"
+#written Off
+sheet[f"A{maxRows+1}"] = "Written Off"
+sheet[f"B{maxRows+1}"] = f"=SUMIFS(I2:I{maxRows},F2:F{maxRows},FALSE)"
+#percent written off
+sheet[f"C{maxRows+1}"] = f"=B{maxRows+1}/B{maxRows+4}"
+sheet[f"C{maxRows+1}"].number_format = "0.00%"
+
+#Retained
+sheet[f"A{maxRows+2}"] = "Collected"
+sheet[f"B{maxRows+2}"] = f"=SUMIFS(I2:I{maxRows},F2:F{maxRows},TRUE,K2:K{maxRows},\"=0\")"
+#percent Retained(collected)
+sheet[f"C{maxRows+2}"] = f"=B{maxRows+2}/B{maxRows+4}"
 sheet[f"C{maxRows+2}"].number_format = "0.00%"
-#retained
-sheet[f"A{maxRows+3}"] = "Retained "
-sheet[f"B{maxRows+3}"] = f"=B{maxRows+1}-B{maxRows+2}"
-sheet[f"C{maxRows+3}"] = f"=B{maxRows+3}/B{maxRows+1}"
+
+#AB Due
+sheet[f"A{maxRows+3}"] = "In Collections"
+sheet[f"B{maxRows+3}"] = f"=SUMIFS(I2:I{maxRows},F2:F{maxRows},TRUE,K2:K{maxRows},\"<>0\")"
+#percent AB Due
+sheet[f"C{maxRows+3}"] = f"=B{maxRows+3}/B{maxRows+4}"
 sheet[f"C{maxRows+3}"].number_format = "0.00%"
 
-#fix nested IF
-sheet.formula_attributes[f"B{maxRows+2}"] = {'t':'array', 'ref': f'B{maxRows+2}'}
+#total Billed
+sheet[f"A{maxRows+4}"] = "Total Billed"
+sheet[f"B{maxRows+4}"] = f"=SUM(I2:I{maxRows})"
 
-
-#sheet.append(["Retained",f"=B{maxRows+1}-B{maxRows+2}"])
 
 workbook.save(workbookFilePath)
 workbook.close
